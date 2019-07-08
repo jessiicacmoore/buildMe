6d47046e9ed52e316e06a4abb2233b11b5cf3369
Jessica Moore <jessica.cmoore1@gmail.com>
2019-07-08 13:59:10 -0400

    Defined Business Profile model

--- backend/projects/models.py
+++ backend/projects/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Business_Profile(models.Model):
+  user = models.OneToOneField(
+    User,
+    on_delete=models.CASCADE,
+    primary_key=True
+  )
+  name = models.CharField(max_length=255)
+  desc = models.TextField()
+  current_site = models.URLField(blank=True)
+
+  def __str__(self):
+    return f"{self.name}"
+
+
+
