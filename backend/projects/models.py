098115191d6b252632cb0fb4a041d833e9bddda5
Jessica Moore <jessica.cmoore1@gmail.com>
2019-07-08 15:04:03 -0400

    Implemented project model

--- backend/projects/models.py
+++ backend/projects/models.py
@@ -14,8 +14,20 @@ class CustomUser(AbstractUser):
   github = models.URLField(blank=True)
 
   def __str__(self):
-    return self.email
+    return f"{self.name}, {self.email}"
 
-
-
+class Project(models.Model):
+  TYPE_CHOICES = [
+    ('maintenance', 'Maintenance'),
+    ('full_project', 'Full Project')
+  ]
+  owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="projects")
+  title = models.CharField(max_length=255)
+  description = models.TextField()
+  project_type = models.CharField(max_length=50, choices=TYPE_CHOICES, default='full_project')
+  is_draft = models.BooleanField(default=True)
+  creation_date = models.DateField(auto_now_add=True)
+  published_date = models.DateField(blank=true)
+  
+  
 
