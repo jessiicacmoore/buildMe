bbe2f96df8c4898159e3cb2b1cce28c05be6a395
Jessica Moore <jessica.cmoore1@gmail.com>
2019-07-08 15:13:42 -0400

    Registered Application model with admin

--- backend/projects/admin.py
+++ backend/projects/admin.py
@@ -2,9 +2,8 @@ from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
 
-from.models import CustomUser, Project
+from.models import CustomUser, Project, Application
 
 admin.site.register(CustomUser, UserAdmin)
 admin.site.register(Project)
-
-# Register your models here.
+admin.site.register(Application)
