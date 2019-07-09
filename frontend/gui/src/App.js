396c9a85627b11b40dde5ea962fc9e13b41daf13
Jessica Moore <jessica.cmoore1@gmail.com>
2019-07-09 15:36:18 -0400

    Installed axios to frontend and did a sanity check to ensure requests can be made to django api

--- frontend/gui/src/App.js
+++ frontend/gui/src/App.js
@@ -1,25 +1,28 @@
-import React from 'react';
-import './App.css';
+import React, {useState, useEffect} from "react";
+import "./App.css";
+import axios from "axios";
 
-function App() {
+const App = () => {
+
+  const[projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    axios
+    .get('http://127.0.0.1:8000/api/projects')
+    .then(response => {
+      console.log(response.data);
+      setProjects(response.data)
+    })
+  }, [])
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>hello world</h1>
+      <button onClick={() => console.log('hello')}>hello</button>
+      <button onClick={() => console.log(projects)}>!!!</button>
     </div>
   );
-}
+};
 
 export default App;
