2608d5d7f399dd6bf874853ea10fc0bf4564f4f0
Jessica Moore <jessica.cmoore1@gmail.com>
2019-07-09 21:36:22 -0400

    Implementing Splash Page + Styling

--- frontend/gui/src/App.js
+++ frontend/gui/src/App.js
@@ -1,27 +1,41 @@
 import React, {useState, useEffect} from "react";
-import "./App.css";
+import "./app.scss";
+import hero from "./hero.jpg";
 import axios from "axios";
 
 const App = () => {
 
   const[projects, setProjects] = useState([]);
 
-  useEffect(() => {
-    axios
-    .get('http://127.0.0.1:8000/api/projects')
-    .then(response => {
-      console.log(response.data);
-      setProjects(response.data)
-    })
-  }, [])
+  // useEffect(() => {
+  //   axios
+  //   .get('http://127.0.0.1:8000/api/projects')
+  //   .then(response => {
+  //     console.log(response.data);
+  //     setProjects(response.data)
+  //   })
+  // }, [])
 
   
   return (
-    <div className="App">
-      <h1>hello world</h1>
-      <button onClick={() => console.log('hello')}>hello</button>
-      <button onClick={() => console.log(projects)}>!!!</button>
-    </div>
+    <header className="app splash-page-header wrapper">
+      <nav className="splash-page-nav">
+        <h1 className="logo">build<span>Me</span></h1>
+        <ul className="nav-links">
+          <li className="nav-item">About</li>
+          <li className="nav-item">Sign In</li>
+          <li className="nav-item">Sign Up</li>
+        </ul>
+      </nav>
+      <div className="hero-section">
+        <div className="text-container">
+          <h2>Lorem ipsum dolor sit amet consectetur.</h2>
+          <a href="" className="btn btn-full">Sign Up</a>
+          <a href="" className="btn btn-ghost">Projects</a>
+        </div>
+        <img src={hero} alt="buildMe"/>
+      </div>
+    </header>
   );
 };
 
