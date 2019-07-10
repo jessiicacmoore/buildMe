8414b3486f8e5f7dc7884f9ddeeae8382579c69e
TIMUR <1tkurbanov@gmail.com>
2019-07-10 15:58:52 -0400

    form

--- frontend/gui/src/App.js
+++ frontend/gui/src/App.js
@@ -1,43 +1,15 @@
-import React, {useState, useEffect} from "react";
-import hero from "./hero.jpg";
-import axios from "axios";
-
-import './styles/app.scss'
+import React from 'react';
+import Landing from './Components/Landing';
+import SignInForm from './Components/SignInForm';
+import 'antd/dist/antd.css';
 
 const App = () => {
-
-  const[projects, setProjects] = useState([]);
-
-  // useEffect(() => {
-  //   axios
-  //   .get('http://127.0.0.1:8000/api/projects')
-  //   .then(response => {
-  //     console.log(response.data);
-  //     setProjects(response.data)
-  //   })
-  // }, [])
-
-  
   return (
-    <header className="app splash-page-header wrapper">
-      <nav className="splash-page-nav">
-        <h1 className="logo">build<span>Me</span></h1>
-        <ul className="nav-links">
-          <li className="nav-item">About</li>
-          <li className="nav-item">Sign In</li>
-          <li className="nav-item">Sign Up</li>
-        </ul>
-      </nav>
-      <div className="hero-section">
-        <div className="text-container">
-          <h2>Lorem ipsum dolor sit amet consectetur.</h2>
-          <a href="" className="btn btn-full">Sign Up</a>
-          <a href="" className="btn btn-ghost">Projects</a>
-        </div>
-        <img src={hero} alt="buildMe"/>
-      </div>
-    </header>
-  );
+    <div>
+      <Landing />
+      <SignInForm />
+    </div>
+  )
 };
 
 export default App;
