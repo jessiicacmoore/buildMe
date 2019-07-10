15111ea169addde04a7ba5f46bdf7271ce0348da
Jessica Moore <jessica.cmoore1@gmail.com>
2019-07-10 12:44:32 -0400

    Defined CRUD api views

--- backend/projects/api/serializers.py
+++ backend/projects/api/serializers.py
@@ -1,43 +1,68 @@
 from rest_framework import serializers
 from projects.models import *
 
-class PublicProfileSerializer(serializers.ModelSerializer):
+
+class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ('first_name', 'last_name', 'profile', 'profile_picture', 'description', 'website', 'linkedin', 'github')
+        fields = (
+            'id',
+            'username',
+            'email',
+            'password',
+            'first_name',
+            'last_name',
+            'profile',
+            'profile_picture',
+            'description',
+            'website',
+            'linkedin',
+            'github',
+            'applications',
+            'projects'
+        )
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = (
+            'id',
+            'owner',
+            'title',
+            'description',
+            'project_type',
+            'is_draft',
+            'creation_date',
+            'published_date',
+            'applications',
+            'tasks'
+        )
 
 class ApplicationSerializer(serializers.ModelSerializer):
-    applicant = PublicProfileSerializer(many=False, read_only=True)
     class Meta:
         model = Application 
-        fields = ('applicant', 'project', 'cover_letter', 'is_hired')
+        fields = (
+            'id',
+            'applicant',
+            'project',
+            'cover_letter',
+            'is_hired'
+        )
 
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
-        fields = ('message', 'stage')
-
+        fields = (
+            'id',
+            'project',
+            'message',
+            'stage'
+        )
 
 # class ChatSerializer(serializers.ModelSerializer):
 #     class Meta:
 #         model = Chat
 #         fields = ('user', 'message', 'timestamp')
  
-class ProjectSerializer(serializers.ModelSerializer):
-    owner = PublicProfileSerializer(many=False, read_only=True)
-    applications = ApplicationSerializer(many=True, read_only=True)
-    tasks = TaskSerializer(many=True, read_only=True)
-
-    class Meta:
-        model = Project
-        fields = ('owner', 'title', 'description', 'project_type', 'tasks', 'is_draft', 'creation_date', 'published_date', 'applications')
-
-class ProfileSerializer(serializers.ModelSerializer):
-    applications = ApplicationSerializer(many=True, read_only=True)
-    projects = ProjectSerializer(many=True, read_only=True)
-
-    class Meta:
-        model = CustomUser
-        fields = ('first_name', 'last_name', 'profile', 'profile_picture', 'description', 'website', 'linkedin', 'github', 'projects', 'applications')
 
 
--- backend/projects/api/views.py
+++ backend/projects/api/views.py
@@ -1,15 +1,18 @@
-from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView
 from projects.models import CustomUser, Project, Application, Chat, Task
 from .serializers import *
 
-class ProjectListView(ListAPIView):
-  queryset = Project.objects.all()
-  serializer_class = ProjectSerializer
 
-class ProjectDetailView(RetrieveAPIView):
-  queryset = Project.objects.all()
-  serializer_class = ProjectSerializer
+# ------------------------------
+# PROFILE API VIEWS
+# ------------------------------
 
+# --- CREATE
+class ProfileCreateView(CreateAPIView):
+  queryset = CustomUser.objects.all()
+  serializer_class = ProfileSerializer
+
+# --- RETREIVE
 class ProfileListView(ListAPIView):
   queryset = CustomUser.objects.all()
   serializer_class = ProfileSerializer
@@ -18,21 +21,102 @@ class ProfileDetailView(RetrieveAPIView):
   queryset = CustomUser.objects.all()
   serializer_class = ProfileSerializer
 
-# class ApplicationListView(ListAPIView):
-#   queryset = Application.objects.all()
-#   serializer_class = ApplicationSerializer
+# --- UPDATE
+class ProfileUpdateView(UpdateAPIView):
+  queryset = CustomUser.objects.all()
+  serializer_class = ProfileSerializer
 
-# class ApplicationDetailView(RetrieveAPIView):
-#   queryset = Application.objects.all()
-#   serializer_class = ApplicationSerializer
+# --- DESTROY
+class ProfileDestroyView(DestroyAPIView):
+  queryset = CustomUser.objects.all()
+  serializer_class = ProfileSerializer
 
-# class TaskListView(ListAPIView):
-#   queryset = Task.objects.all()
-#   serializer_class = TaskSerializer
 
-# class TaskDetailView(RetrieveAPIView):
-#   queryset = Task.objects.all()
-#   serializer_class = TaskSerializer
+# ------------------------------
+# PROJECT API VIEWS
+# ------------------------------
+
+# --- CREATE
+class ProjectCreateView(CreateAPIView):
+  queryset = Project.objects.all()
+  serializer_class = ProjectSerializer
+
+# --- RETREIVE
+class ProjectListView(ListAPIView):
+  queryset = Project.objects.all()
+  serializer_class = ProjectSerializer
+
+class ProjectDetailView(RetrieveAPIView):
+  queryset = Project.objects.all()
+  serializer_class = ProjectSerializer
+
+# --- UPDATE
+class ProjectUpdateView(UpdateAPIView):
+  queryset = Project.objects.all()
+  serializer_class = ProjectSerializer
+
+# --- DESTROY
+class ProjectDestroyView(DestroyAPIView):
+  queryset = Project.objects.all()
+  serializer_class = ProjectSerializer
+
+
+# ------------------------------
+# APPLICATION API VIEWS
+# ------------------------------
+
+# --- CREATE
+class ApplicationCreateView(CreateAPIView):
+  queryset = Application.objects.all()
+  serializer_class = ApplicationSerializer
+
+# --- RETREIVE
+class ApplicationListView(ListAPIView):
+  queryset = Application.objects.all()
+  serializer_class = ApplicationSerializer
+
+class ApplicationDetailView(RetrieveAPIView):
+  queryset = Application.objects.all()
+  serializer_class = ApplicationSerializer
+
+# --- UPDATE
+class ApplicationUpdateView(UpdateAPIView):
+  queryset = Application.objects.all()
+  serializer_class = ApplicationSerializer
+
+# --- DESTROY
+class ApplicationDestroyView(DestroyAPIView):
+  queryset = Application.objects.all()
+  serializer_class = ApplicationSerializer
+
+
+# ------------------------------
+# TASK API VIEWS
+# ------------------------------
+
+# --- CREATE
+class TaskCreateView(CreateAPIView):
+  queryset = Task.objects.all()
+  serializer_class = TaskSerializer
+
+# --- RETRIEVE
+class TaskListView(ListAPIView):
+  queryset = Task.objects.all()
+  serializer_class = TaskSerializer
+
+class TaskDetailView(RetrieveAPIView):
+  queryset = Task.objects.all()
+  serializer_class = TaskSerializer
+
+# --- UPDATE
+class TaskUpdateView(UpdateAPIView):
+  queryset = Task.objects.all()
+  serializer_class = TaskSerializer
+
+# --- DESTROY
+class TaskDestroyView(DestroyAPIView):
+  queryset = Task.objects.all()
+  serializer_class = TaskSerializer
 
 # class ChatListView(ListAPIView):
 #   queryset = Chat.objects.all()
