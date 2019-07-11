36729a56bb32314c4a62f20610e32492fdf90550
Jessica Moore <jessica.cmoore1@gmail.com>
2019-07-11 19:32:34 -0400

    Josh helped fix mapping

--- frontend/gui/src/Components/ProjectList.js
+++ frontend/gui/src/Components/ProjectList.js
@@ -1,24 +1,58 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ProjectListItem from "./ProjectListItem";
-import axios from "axios"
+import axios from "axios";
 
 import "./styles/project-list.scss";
 
 const ProjectList = () => {
-  // temporary project until we have cors figured out
+  
   const url = "http://localhost:8000/api/project/"
-  const allProjects = axios.get(url).then(response => {
-    console.log(response.data)
-  });
+  const [allProjects, setAllProjects] = useState([]);
+  // const [displayProjects, setDisplayProjects] = useState([]);
+
+  useEffect(() => {
+    getAllProjects()  // const ListItem = ({ item }) => (
+      //   <li>
+      //     <div>{item.title}</div>
+      //   </li>
+      // );;
+    // setDisplayProjects(allProjects);
+  }, [])
+
+  const getAllProjects = async (filter_criteria) => {
+    const base_url = "http://localhost:8000/api/project/"
+    const url = base + filter_criteria // "?tags=asdf,asdfads,asdfds"
+
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data)
+    setAllProjects(data);
+  }
+
+  const projectRows = allProjects.map((project, i) => {
+    console.log("Looping.....", project)
+    return (
+      <h1>Eyyyy {project.title}</h1>  
+    )
+  }); 
+
+  // const ListItem = ({ item }) => (
+  //   <li>
+  //     <div>{item.title}</div>
+  //   </li>
+  // );
 
   return (
     <div className="project-listing-container">
-      <ul className="project-list">
-        
+      <ul className="project-list" >
+        <h1>Yoso</h1>
+        {projectRows}
+        {/* {allProjects.map(item => (
+            <ListItem key={item.id} item={item} />
+        ))} */}
       </ul>
     </div>
   );
 };
-
 
 export default ProjectList;
