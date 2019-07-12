2f503894fe5bf08d149ad460f33c356fc79aa680
Jessica Moore <jessica.cmoore1@gmail.com>
2019-07-12 11:07:05 -0400

    Successfully retreiving data from API

--- frontend/gui/src/Components/ProjectList.js
+++ frontend/gui/src/Components/ProjectList.js
@@ -1,58 +1,28 @@
 import React, { useEffect, useState } from "react";
 import ProjectListItem from "./ProjectListItem";
-import axios from "axios";
 
 import "./styles/project-list.scss";
 
 const ProjectList = () => {
-  
-  const url = "http://localhost:8000/api/project/"
   const [allProjects, setAllProjects] = useState([]);
-  // const [displayProjects, setDisplayProjects] = useState([]);
 
   useEffect(() => {
-    getAllProjects()  // const ListItem = ({ item }) => (
-      //   <li>
-      //     <div>{item.title}</div>
-      //   </li>
-      // );;
-    // setDisplayProjects(allProjects);
-  }, [])
+    getAllProjects();
+  }, []);
 
-  const getAllProjects = async (filter_criteria) => {
-    const base_url = "http://localhost:8000/api/project/"
-    const url = base + filter_criteria // "?tags=asdf,asdfads,asdfds"
-
-    const response = await fetch(url);
+  const getAllProjects = async filter_criteria => {
+    const base_url = "http://localhost:8000/api/project/";
+    // const url = base_url + filter_criteria;
+    const response = await fetch(base_url);
     const data = await response.json();
-    console.log(data)
     setAllProjects(data);
-  }
+  };
 
   const projectRows = allProjects.map((project, i) => {
-    console.log("Looping.....", project)
-    return (
-      <h1>Eyyyy {project.title}</h1>  
-    )
-  }); 
+    return <ProjectListItem key={project.id} project={project} />;
+  });
 
-  // const ListItem = ({ item }) => (
-  //   <li>
-  //     <div>{item.title}</div>
-  //   </li>
-  // );
-
-  return (
-    <div className="project-listing-container">
-      <ul className="project-list" >
-        <h1>Yoso</h1>
-        {projectRows}
-        {/* {allProjects.map(item => (
-            <ListItem key={item.id} item={item} />
-        ))} */}
-      </ul>
-    </div>
-  );
+  return <ul>{projectRows}</ul>;
 };
 
 export default ProjectList;
